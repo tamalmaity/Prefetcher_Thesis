326705505f51c79c8d7b81e4bf8087e9b9bb4805
Tamal Maity <tamaldeepmaity@gmail.com>
2022-03-30 16:41:19 +0530

    Add files via upload

---
+++ Pythia_python/main_run.sh
@@ -0,0 +1,50 @@
+./new_run.sh /home/tamalmaity/data/LoadTraces/spec06/462.libquantum-s1.txt.xz
+cp ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/libquantum-s1_1.txt
+cp ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/libquantum-s1_2.txt
+cp LOGS.log /home/tamalmaity/mlsysarch/PYTHIA/LOGS/LOGS_libquantum-s1.txt
+
+./new_run.sh /home/tamalmaity/data/LoadTraces/spec06/462.libquantum-s2.txt.xz
+cp ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/libquantum-s2_1.txt
+cp ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/libquantum-s2_2.txt
+cp LOGS.log /home/tamalmaity/mlsysarch/PYTHIA/LOGS/LOGS_libquantum-s2.txt
+
+
+./new_run.sh /home/tamalmaity/data/LoadTraces/spec06/410.bwaves-s0.txt.xz
+cp ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/bwaves-s0_1.txt
+cp ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/bwaves-s0_2.txt
+cp LOGS.log /home/tamalmaity/mlsysarch/PYTHIA/LOGS/LOGS_bwaves-s0.txt
+
+./new_run.sh /home/tamalmaity/data/LoadTraces/spec06/410.bwaves-s1.txt.xz
+cp ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/bwaves-s1_1.txt
+cp ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/bwaves-s1_2.txt
+cp LOGS.log /home/tamalmaity/mlsysarch/PYTHIA/LOGS/LOGS_bwaves-s1.txt
+
+./new_run.sh /home/tamalmaity/data/LoadTraces/spec06/410.bwaves-s2.txt.xz
+cp ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/bwaves-s2_1.txt
+cp ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/bwaves-s2_2.txt
+cp LOGS.log /home/tamalmaity/mlsysarch/PYTHIA/LOGS/LOGS_bwaves-s2.txt
+
+
+./new_run.sh /home/tamalmaity/data/LoadTraces/spec06/470.lbm-s0.txt.xz
+cp ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/lbm-s0_1.txt
+cp ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/lbm-s0_2.txt
+cp LOGS.log /home/tamalmaity/mlsysarch/PYTHIA/LOGS/LOGS_lbm-s0.txt
+
+./new_run.sh /home/tamalmaity/data/LoadTraces/spec06/470.lbm-s1.txt.xz
+cp ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_1.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/lbm-s1_1.txt
+cp ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches
+mv /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/ip_addr_105_output_2.txt /home/tamalmaity/mlsysarch/PYTHIA/Prefetches/lbm-s1_2.txt
+cp LOGS.log /home/tamalmaity/mlsysarch/PYTHIA/LOGS/LOGS_lbm-s1.txt
